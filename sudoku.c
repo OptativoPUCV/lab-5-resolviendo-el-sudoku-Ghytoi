bf87ac7ca65770d8f68f501a69fb016479cfbbab
Your Name <you@example.com>
2025-05-16 14:15:27 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -62,7 +62,7 @@ List* get_adj_nodes(Node* n){
         {
           for (control3 = 0; control3 < 9; control3++)
           {
-            Node *nodoNuevo = copy(n);
+            Node *nodoNuevo = copy(n + 1);
             pushBack(list, nodoNuevo);
           }
         }
